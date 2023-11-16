56494e3ddaa9334b1f582075b1473b12204e6773
Maxim Ponomarev <ponomarevmv8@yandex.ru>
2023-11-16 03:58:41 +0300

    fix: Getters and Setters for Entity
    Вместо аннотации Data из ламбока в ручную прописаны гетеры и сетеры, для устранения ошибки hasCode

--- src/main/java/com/javarush/domain/entity/City.java
+++ src/main/java/com/javarush/domain/entity/City.java
@@ -3,9 +3,10 @@ package com.javarush.domain.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Entity
 @Table(schema = "world", name = "city")
-@Data
 public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +22,43 @@ public class City {
 
     private Integer population;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
 }
--- src/main/java/com/javarush/domain/entity/CountryLanguage.java
+++ src/main/java/com/javarush/domain/entity/CountryLanguage.java
@@ -8,7 +8,6 @@ import java.math.BigDecimal;
 
 @Entity
 @Table(schema = "world", name = "country_language")
-@Data
 public class CountryLanguage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,4 +26,43 @@ public class CountryLanguage {
 
     private BigDecimal percentage;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Boolean getOfficial() {
+        return isOfficial;
+    }
+
+    public void setOfficial(Boolean official) {
+        isOfficial = official;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
 }
